a2fc4d8ebac5520f588b2467d15abff03750b709
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-06-09 06:51:31 +0100

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,6 @@
+var ukrGoodbyeAudio = new Audio('audio/ukrGoodbye.m4a');
+var ukrHelloAudio = new Audio('audio/ukrHello.m4a');
+var ukrPleaseAudio = new Audio('audio/ukrPlease.m4a');
+var ukrSorryAudio = new Audio('audio/ukrSorry.m4a');
+var ukrThankYouAudio = new Audio('audio/ukrThankYou.m4a');
+var ukrWhatAudio = new Audio('audio/ukrWhat.m4a');
