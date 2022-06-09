f1ac83e5c043c2588264c6e69e16e61176fb1229
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-06-09 16:20:59 +0100

    Add files via upload

--- JS.js
+++ JS.js
@@ -4,3 +4,10 @@ var ukrPleaseAudio = new Audio('audio/ukrPlease.m4a');
 var ukrSorryAudio = new Audio('audio/ukrSorry.m4a');
 var ukrThankYouAudio = new Audio('audio/ukrThankYou.m4a');
 var ukrWhatAudio = new Audio('audio/ukrWhat.m4a');
+
+var engGoodbyeAudio = new Audio('audio/engGoodbye.mp3');
+var engHelloAudio = new Audio('audio/engHello.mp3');
+var engPleaseAudio = new Audio('audio/engPlease.mp3');
+var engSorryAudio = new Audio('audio/engSorry.mp3');
+var engThankYouAudio = new Audio('audio/engThankYou.mp3');
+var engWhatAudio = new Audio('audio/engWhat.mp3');
